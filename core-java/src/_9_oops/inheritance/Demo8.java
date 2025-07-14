d9040e11901fc286e34a9508c1f596f369fe053e
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2025-07-14 10:40:05 +0530

    inheritance

---
+++ core-java/src/_9_oops/inheritance/Demo8.java
@@ -0,0 +1,21 @@
+package _9_oops.inheritance;
+
+public class Demo8 {
+    public static void main(String[] args) {
+        class Parent {
+            int x = 1;
+        }
+
+        class Child extends Parent {
+            int x = 2;
+            int y = 3;
+        }
+        //01 . Parent ref = new Parent();
+        // we can access only parent class members and not child class
+        Parent parentRef = new Parent();
+        System.out.println("x = " + parentRef.x);//1
+        //System.out.println("y = " + parentRef.y);//CTE
+
+
+    }
+}
