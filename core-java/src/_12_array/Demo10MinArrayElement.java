bdb2257af40045ccde6242ef986dd86b5cc6e8df
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2025-08-07 12:46:50 +0530

    array

---
+++ core-java/src/_12_array/Demo10MinArrayElement.java
@@ -0,0 +1,38 @@
+package _12_array;
+
+public class Demo10MinArrayElement {
+    public static void main(String[] args) {
+        int[] array = {16, 18, 11, 12, 13, 14, 15};
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("Minimum element is " + min);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ core-java/src/_12_array/Demo3.java
@@ -0,0 +1,8 @@
+package _12_array;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        int[] array = new int[-10];
+        System.out.println(array.length);
+    }
+}
