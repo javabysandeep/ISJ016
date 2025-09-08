f60198a69034b7893946e8e547ea016b317f4478
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2025-09-08 12:25:17 +0530

    exception handling

--- core-java/src/_13_string_handling/Demo17StringBuilder.java
+++ core-java/src/_13_string_handling/Demo17StringBuilder.java
@@ -3,6 +3,7 @@ package _13_string_handling;
 public class Demo17StringBuilder {
     public static void main(String[] args) {
         StringBuilder stringBuilder = new StringBuilder("Hello World");
+        System.out.println(10/0);
         stringBuilder.append(" there");
         stringBuilder.insert(0,"Hi");
         stringBuilder.delete(3,6);
